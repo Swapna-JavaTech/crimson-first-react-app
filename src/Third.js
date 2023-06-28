4e49efc5ea506940c1fd1cb9c607046e422bf354
Swapna Reniguntla <110802759+Swapna-JavaTech@users.noreply.github.com>
2023-06-28 14:18:52 +0530

    Create Third.js

---
+++ src/Third.js
@@ -0,0 +1,16 @@
+import mvc from './mvc.png';
+import './App.css';
+
+function Third() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={mvc} className="App-logo" alt="mvc" />
+
+
+      </header>
+    </div>
+  );
+}
+
+export default Third;
